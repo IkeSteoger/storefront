1dbfe6a311350fab09bf0bb49cc34f438ff55503
MICHAEL STEOGER <ikesteoger@gmail.com>
2023-07-07 16:04:59 -0700

    lab 37 bopped

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,6 +1,26 @@
+import { useSelector } from 'react-redux';
+import { AppBar, Button, Container, Toolbar, Typography } from '@mui/material';
+
 function Header() {
+
+    const { cart } = useSelector((state) => state.cart);
+    // console.log(cartCount)
+
     return (
-        <h1>The Storefront of Practicing</h1>
+        <AppBar position='static' sx={{ backgroundColor: 'white' }}>
+            <Container>
+                <Toolbar disableGutters>
+                    <Typography
+                        variant='h4'
+                        sx={{ color: 'black' }}>
+                        The Storefront of Practicing
+                    </Typography>
+                    <Button>
+                        Cart ({cart.length})
+                    </Button>
+                </Toolbar>
+            </Container>
+        </AppBar>
     )
 }
 
